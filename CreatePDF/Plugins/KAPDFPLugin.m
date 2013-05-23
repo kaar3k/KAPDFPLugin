e661f9c34b4fcbb125164aa1d24c28756dfbe3d0
karthik.m <karthik.m@zeroonetech.net>
2013-05-23 19:01:46 +0530

    Initial commit

---
+++ CreatePDF/Plugins/KAPDFPLugin.m
@@ -0,0 +1,104 @@
+//
+//  KAPDFPLugin.m
+//  CreatePDF
+//
+//  Created by Karthik M on 23/05/13.
+//
+//
+
+#import "KAPDFPLugin.h"
+#import <QuartzCore/QuartzCore.h>
+
+#define PDFWIDTH 612
+
+@implementation KAPDFPLugin
+@synthesize webViewHeight;
+@synthesize imageName;
+
+#pragma mark - Plugin Signature
+
+- (void)generatePDF:(CDVInvokedUrlCommand*)command
+{
+    [self.commandDelegate runInBackground:^{
+        if ([self createPDFromCurrentWebView]) {
+            
+        }
+    }];
+}
+
+
+#pragma mark - PDF 
+
+- (BOOL)createPDFromCurrentWebView
+{
+    webViewHeight = [[self.webView stringByEvaluatingJavaScriptFromString:@"document.body.scrollHeight;"]
+                     integerValue];
+    CGRect screenRect = self.webView.frame;
+    double currentWebViewHeight = webViewHeight;
+    while (currentWebViewHeight > 0)
+    {
+        imageName ++;
+        
+        UIGraphicsBeginImageContext(screenRect.size);
+
+        [self.webView.layer renderInContext:UIGraphicsGetCurrentContext()];
+        UIImage *newImage = UIGraphicsGetImageFromCurrentImageContext();
+                UIGraphicsEndImageContext();
+        
+        NSString *pngPath = [NSTemporaryDirectory() stringByAppendingPathComponent:[NSString stringWithFormat:@"%d.png",imageName]];
+        
+        CGFloat viewHeight=self.webView.frame.size.height;
+        if(currentWebViewHeight < viewHeight)
+        {
+            CGRect lastImageRect = CGRectMake(0, viewHeight - currentWebViewHeight, self.webView.frame.size.width, currentWebViewHeight);
+            CGImageRef imageRef = CGImageCreateWithImageInRect([newImage CGImage], lastImageRect);
+            
+            newImage = [UIImage imageWithCGImage:imageRef];
+            CGImageRelease(imageRef);
+        }
+        
+        NSError *error=nil;
+        BOOL result=[UIImagePNGRepresentation(newImage)
+                                writeToFile:pngPath
+                            options:NSDataWritingAtomic error:&error];
+        
+        NSString *scrollJS=[NSString stringWithFormat:@"window.scrollBy(0,%f);",viewHeight];
+        if (!result) {
+            return result;
+        }
+        
+        [self.webView performSelectorOnMainThread:@selector(stringByEvaluatingJavaScriptFromString:)
+                                       withObject:scrollJS waitUntilDone:YES];
+        currentWebViewHeight -= viewHeight;
+    }
+    [self drawPdf];
+    return YES;
+}
+
+- (void) drawPdf
+{
+    CGSize pageSize = CGSizeMake(PDFWIDTH, webViewHeight);
+    NSString *fileName = @"Demo.pdf";
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *documentsDirectory = [paths objectAtIndex:0];
+    NSString *pdfFileName = [documentsDirectory stringByAppendingPathComponent:fileName];
+    UIGraphicsBeginPDFContextToFile(pdfFileName, CGRectZero, nil);
+    
+    // Mark the beginning of a new page.
+    UIGraphicsBeginPDFPageWithInfo(CGRectMake(0, 0, pageSize.width, pageSize.height), nil);
+    
+    double currentHeight = 0.0;
+    for (int index = 1; index  <= imageName ; index++)
+    {
+        NSString *pngPath = [NSTemporaryDirectory() stringByAppendingPathComponent:[NSString stringWithFormat:@"%d.png", index]];
+        NSLog(@"%@",pngPath);
+        UIImage *pngImage = [UIImage imageWithContentsOfFile:pngPath];
+        
+        [pngImage drawInRect:CGRectMake(0, currentHeight, pageSize.width, pngImage.size.height)];
+        currentHeight += pngImage.size.height;
+    }
+    
+    UIGraphicsEndPDFContext();
+}
+
+@end
